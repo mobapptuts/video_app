3e9dd1e45b1516abb91a3c80694b0f151e83af93
mobapptuts <nige.henshaw@gmail.com>
2015-12-02 12:56:01 +0900

    Marshmallow runtime permission support

--- app/src/main/java/nigelhenshaw/com/videoapp/VideoAppMainActivity.java
+++ app/src/main/java/nigelhenshaw/com/videoapp/VideoAppMainActivity.java
@@ -1,14 +1,19 @@
 package nigelhenshaw.com.videoapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 
@@ -17,6 +22,8 @@ public class VideoAppMainActivity extends Activity {
     private Button mRecordView, mPlayView;
     private VideoView mVideoView;
     private int ACTIVITY_START_CAMERA_APP = 0;
+    private final static int REQUEST_READ_STORAGE_RESULT = 1;
+    private Uri mVideoUri;
 
 
     @Override
@@ -48,8 +55,37 @@ public class VideoAppMainActivity extends Activity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
-            Uri videoUri = data.getData();
-            mVideoView.setVideoURI(videoUri);
+            mVideoUri = data.getData();
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    mVideoView.setVideoURI(mVideoUri);
+                } else {
+                    if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        Toast.makeText(this,
+                                "Need access to file to play the video", Toast.LENGTH_SHORT).show();
+                    }
+                    requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_READ_STORAGE_RESULT);
+                }
+            } else {
+                mVideoView.setVideoURI(mVideoUri);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == REQUEST_READ_STORAGE_RESULT) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mVideoView.setVideoURI(mVideoUri);
+            } else {
+                Toast.makeText(this,
+                        "Cannot play video because we don't have permission to access the file",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
